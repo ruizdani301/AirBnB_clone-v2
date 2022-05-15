424a22459df40117624e9830a3237ae304811a7f
cristian <cristaker1927@hotmail.com>
2022-05-15 14:24:04 -0700

    test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""test for file storage"""
+import unittest
+import pep8
+import json
+import os
+from os import getenv
+import MySQLdb
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+
+
+@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
+class TestDBStorage(unittest.TestCase):
+    """this will test the DBStorage"""
+
+    @classmethod
+    def setUpClass(self):
+        """set up for test"""
+        self.User = getenv("HBNB_MYSQL_USER")
+        self.Passwd = getenv("HBNB_MYSQL_PWD")
+        self.Db = getenv("HBNB_MYSQL_DB")
+        self.Host = getenv("HBNB_MYSQL_HOST")
+        self.db = MySQLdb.connect(host=self.Host, user=self.User,
+                                  passwd=self.Passwd, db=self.Db,
+                                  charset="utf8")
+        self.query = self.db.cursor()
+        self.storage = DBStorage()
+        self.storage.reload()
+
+
