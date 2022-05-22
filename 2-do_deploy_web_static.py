aecb06fc446f3e236f9b402886acc98627c4cb9b
tu nombre <ruizdani301@gmail.com>
2022-05-21 23:54:24 -0500

    all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Fabric script hat distributes an archive to your web servers"""
-from asyncio import run_coroutine_threadsafe
 from fabric.api import put, run, env
 from os.path import exists
 
@@ -19,7 +18,7 @@ def do_deploy(archive_path):
         run('sudo mkdir -p {}{}/'.format(ruta, no_ext))
         run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, ruta, no_ext))
         run('sudo rm /tmp/{}'.format(file_n))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(ruta, no_ext))
         run('sudo rm -rf {}{}/web_static'.format(ruta, no_ext))
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s {}{}/ /data/web_static/current'.format(ruta, no_ext))
