c682ab00f2de82a9310959351396f5c83cbf2c09
tu nombre <ruizdani301@gmail.com>
2022-05-21 22:59:57 -0500

    all

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
-# Configure your Nginx server to have a custom 404 page that contains the string Ceci n'est pas une page.
-
-if [ ! -x /usr/sbin/nginx ];
-then
-    apt-get -y update
-    apt-get -y install nginx
-    ufw allow  'Nginx HTTP' #on port 80
-    service nginx restart
-else
-
-    service nginx restart
-fi
-mkdir -p data/web_static/releases/test
-touch  data/web_static/releases/test/index.html
-echo "<html>
+#bash start
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >> data/web_static/releases/test/index.htm
-sudo ln -sf data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sudo sed -i "/listen 80 default_serve/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
