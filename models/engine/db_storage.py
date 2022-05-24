eb766a97df0e739c8212e5c575c0c8a939d2bd3d
tu nombre <ruizdani301@gmail.com>
2022-05-24 16:05:41 -0500

    all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,7 +68,6 @@ class DBStorage:
         ScopSession = scoped_session(sess1)
         self.__session = ScopSession()
 
-
     def close(self):
         """ close metodo """
         self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ close metodo """
+        self.reload()
